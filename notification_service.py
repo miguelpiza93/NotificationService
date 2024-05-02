6f147518e328dde7299f2f9b015207160dc81300
Miguel Piza <mapiz_930@hotmail.com>
2024-05-02 18:13:44 -0500

    Add Enum to define all notification types Use Enum in __RATE_LIMIT Update the methods and attribute visibility Update the test cases to use the enums

--- notification_service.py
+++ notification_service.py
@@ -1,45 +1,48 @@
 from collections import defaultdict
 from datetime import datetime, timedelta
 
+from notification_type import NotificationType
+
 
 class NotificationService:
+    __RATE_LIMITS = {
+        NotificationType.STATUS: {"interval": timedelta(minutes=1), "limit": 2},
+        NotificationType.NEWS: {"interval": timedelta(days=1), "limit": 1},
+        NotificationType.MARKETING: {"interval": timedelta(hours=1), "limit": 3},
+    }
+
     def __init__(self):
-        self.rate_limits = {
-            "Status": {"interval": timedelta(minutes=1), "limit": 2},
-            "News": {"interval": timedelta(days=1), "limit": 1},
-            "Marketing": {"interval": timedelta(hours=1), "limit": 3},
-        }
-        self.notification_time_by_recipient = defaultdict(lambda: defaultdict(list))
+        self.__notification_time_by_recipient = defaultdict(lambda: defaultdict(list))
 
-    def send_notification(self, recipient, notification_type):
+    def send_notification(self, recipient: str, notification_type: NotificationType) -> bool:
 
-        if notification_type not in self.rate_limits:
+        if notification_type not in self.__RATE_LIMITS:
             print("Not allowed notification type.")
             return False
 
-        limit_info = self.rate_limits[notification_type]
+        limit_info = self.__RATE_LIMITS[notification_type]
         interval = limit_info["interval"]
         limit = limit_info["limit"]
 
-        notification_times = self.notification_time_by_recipient[recipient][notification_type]
+        notification_times = self.__notification_time_by_recipient[recipient][notification_type]
 
         if len(notification_times) < limit:
-            self.add_notification(recipient, notification_type)
+            self.__add_notification(recipient, notification_type)
             return True
 
-        now = datetime.now()
         oldest_notification = notification_times[0]
-        if now - oldest_notification < interval:
-            print(f"Reached limit for {notification_type} - {recipient}.")
+        time_since_oldest_notification = datetime.now() - oldest_notification
+        if time_since_oldest_notification < interval:
+            print(f"Reached limit for {notification_type.value} - {recipient}.")
             return False
 
-        self.remove_oldest_notification(recipient, notification_type)
-        self.add_notification(recipient, notification_type)
+        self.__remove_oldest_notification(recipient, notification_type)
+        self.__add_notification(recipient, notification_type)
         return True
 
-    def add_notification(self, recipient, notification_type):
-        self.notification_time_by_recipient[recipient][notification_type].append(datetime.now())
+    def __add_notification(self, recipient, notification_type):
+        self.__notification_time_by_recipient[recipient][notification_type].append(datetime.now())
 
-    def remove_oldest_notification(self, recipient, notification_type):
-        notification_times = self.notification_time_by_recipient[recipient][notification_type]
+    def __remove_oldest_notification(self, recipient, notification_type):
+        notification_times = self.__notification_time_by_recipient[recipient][notification_type]
         del notification_times[0]
--- test_notification_service.py
+++ test_notification_service.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from unittest.mock import patch
 
 from notification_service import NotificationService
+from notification_type import NotificationType
 
 
 class TestNotificationService(unittest.TestCase):
@@ -11,16 +12,16 @@ class TestNotificationService(unittest.TestCase):
 
     def test_status_limit(self):
         for _ in range(2):
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
-        result = self.notification_service.send_notification("user1@example.com", "Status")
+        result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
         self.assertFalse(result)
 
     def test_status_limit_different_recipient(self):
         for _ in range(2):
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
-        result = self.notification_service.send_notification("user2@example.com", "Status")
+        result = self.notification_service.send_notification("user2@example.com", NotificationType.STATUS)
         self.assertTrue(result)
 
     def test_status_limit_time_interval(self):
@@ -30,41 +31,41 @@ class TestNotificationService(unittest.TestCase):
             mock_datetime.now.return_value = now
 
             # First notification sent at 14:10.0
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
 
             future_time = now + timedelta(seconds=30)
             mock_datetime.now.return_value = future_time
 
             # Second notification sent at 14:10.30
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
 
             future_time = now + timedelta(seconds=60)
             mock_datetime.now.return_value = future_time
 
             # 1 min after first notification
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
 
             future_time = now + timedelta(seconds=80)
             mock_datetime.now.return_value = future_time
 
             # 14:11.20, Missing 10 seconds to be able to send a new notification
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertFalse(result)
 
             # 14:11.30, Ready to send a new notification
             future_time = now + timedelta(seconds=90)
             mock_datetime.now.return_value = future_time
 
-            result = self.notification_service.send_notification("user1@example.com", "Status")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.STATUS)
             self.assertTrue(result)
 
     def test_news_limit(self):
-        result = self.notification_service.send_notification("user1@example.com", "News")
+        result = self.notification_service.send_notification("user1@example.com", NotificationType.NEWS)
         self.assertTrue(result)
-        result = self.notification_service.send_notification("user1@example.com", "News")
+        result = self.notification_service.send_notification("user1@example.com", NotificationType.NEWS)
         self.assertFalse(result)
 
     def test_news_limit_time_interval(self):
@@ -73,20 +74,20 @@ class TestNotificationService(unittest.TestCase):
         with patch('notification_service.datetime') as mock_datetime:
             mock_datetime.now.return_value = now
 
-            result = self.notification_service.send_notification("user1@example.com", "News")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.NEWS)
             self.assertTrue(result)
 
             future_time = now + timedelta(days=1)
             mock_datetime.now.return_value = future_time
 
-            result = self.notification_service.send_notification("user1@example.com", "News")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.NEWS)
             self.assertTrue(result)
 
     def test_marketing_limit(self):
         for _ in range(3):
-            result = self.notification_service.send_notification("user1@example.com", "Marketing")
+            result = self.notification_service.send_notification("user1@example.com", NotificationType.MARKETING)
             self.assertTrue(result)
-        result = self.notification_service.send_notification("user1@example.com", "Marketing")
+        result = self.notification_service.send_notification("user1@example.com", NotificationType.MARKETING)
         self.assertFalse(result)
 
     def test_unsupported_notification(self):
